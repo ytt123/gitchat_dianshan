58da321e7216cf3d4f2bf815b81a3b4e316bd50d
郭方超 <guofangchao@daling.com>
2018-02-23 23:47:59 +0800

    给首页容器添加样式

--- src/home/index.js
+++ src/home/index.js
@@ -4,22 +4,33 @@ import React from 'react';
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    Dimensions
 } from 'react-native';
 import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
 import px from '../utils/px'
-import storage from '../utils/Storage'
+
+const deviceWidth = Dimensions.get('window').width;
+
+
 
 export default class extends React.Component {
     render() {
-        return <View style={{ marginTop: px(500) }}>
-            <Text>这是首页</Text>
+        return <View style={{ flex: 1, backgroundColor: "#fff" }}>
+            <View style={styles.pageView}>
+                
+            </View>
         </View>
     }
     async componentDidMount(){
-        // storage.setItem('a',234)
-        let res=await storage.getItem('a')
-        log(res)
+       
     }
 }
+
+const styles=StyleSheet.create({
+    pageView: {
+        flex: 1,
+        width: deviceWidth
+    },
+})
