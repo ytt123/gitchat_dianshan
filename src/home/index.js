ae0d269ba0b8abfbff2e79ff8cfd4ead8266b802
郭方超 <guofangchao@daling.com>
2018-02-23 19:31:55 +0800

    封装本地存储

--- src/home/index.js
+++ src/home/index.js
@@ -9,11 +9,17 @@ import {
 import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
 import px from '../utils/px'
+import storage from '../utils/Storage'
 
 export default class extends React.Component {
     render() {
         return <View style={{ marginTop: px(500) }}>
             <Text>这是首页</Text>
         </View>
+    }
+    async componentDidMount(){
+        // storage.setItem('a',234)
+        let res=await storage.getItem('a')
+        log(res)
     }
 }
