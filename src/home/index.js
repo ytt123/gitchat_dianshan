7f5702f17d28126d569df58d102adc99fde9f5b2
郭方超 <guofangchao@daling.com>
2018-02-24 00:05:18 +0800

    添加toast并测试

--- src/home/index.js
+++ src/home/index.js
@@ -13,6 +13,7 @@ import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
 import px from '../utils/px'
 import swiper from 'react-native-swiper'
+import toast from '../utils/toast'
 
 const deviceWidth = Dimensions.get('window').width;
 
@@ -48,18 +49,24 @@ export default class extends React.Component {
     render() {
         return <View style={{ flex: 1, backgroundColor: "#fff" }}>
             <View style={styles.pageView}>
-                
+                <TouchableWithoutFeedback onPress={this.test}>
+                    <Text>测试按钮</Text>
+                </TouchableWithoutFeedback>
             </View>
         </View>
     }
     async componentDidMount(){
        
     }
+    test(){
+        toast("测试弹出的toast")
+    }
 }
 
 const styles=StyleSheet.create({
     pageView: {
         flex: 1,
-        width: deviceWidth
+        width: deviceWidth,
+        marginTop: 100,
     },
 })
