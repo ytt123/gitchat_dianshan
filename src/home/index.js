f0cac787ca67a6cbbf4a6e5c2b8b8f0cbfb33088
郭方超 <guofangchao@daling.com>
2018-02-23 23:58:33 +0800

    添加轮播组件

--- src/home/index.js
+++ src/home/index.js
@@ -5,15 +5,44 @@ import {
     StyleSheet,
     View,
     Text,
-    Dimensions
+    Dimensions,
+    TouchableWithoutFeedback,
+    ImageBackground
 } from 'react-native';
 import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
 import px from '../utils/px'
+import swiper from 'react-native-swiper'
 
 const deviceWidth = Dimensions.get('window').width;
 
+/**
+ * Banner和快捷入口
+ */
+class MyBanner extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            banners: [],
+            quicks: [],
+            status: false,
+        }
+    }
+    render(){
+        return <View>
 
+        </View>
+    }
+    componentDidMount(){
+
+    }
+    /**
+     * 刷新列表内容
+     */
+    async refresh(){
+
+    }
+}
 
 export default class extends React.Component {
     render() {
