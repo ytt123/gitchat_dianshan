54e697c2ab95251408d2299c240898ede851afd7
郭方超 <guofangchao@daling.com>
2018-02-24 18:25:24 +0800

    添加顶部组件

--- src/home/index.js
+++ src/home/index.js
@@ -10,7 +10,8 @@ import {
     TouchableOpacity,
     ImageBackground,
     Image,
-    FlatList
+    FlatList,
+    Platform
 } from 'react-native';
 import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
@@ -480,6 +481,111 @@ const goodStyles = StyleSheet.create({
         height: px(33)
     },
 });
+
+class SearchHeader extends React.Component{
+    render() {
+        return <View style={styleSearchBar.header}>
+            <TouchableOpacity activeOpacity={0.9} onPress={() => this.shareTo()}>
+                <View style={styleSearchBar.back}>
+                    <Image source={{ uri: 'http://img.cdn.daling.com/data/files/mobile/img/dalingjia.jpg' }}
+                        style={styleSearchBar.shopLogo} />
+                    <Image style={styleSearchBar.headerShareImg}
+                        source={{ uri: require("../images/icon-index-share") }} />
+                </View>
+            </TouchableOpacity>
+            <TouchableWithoutFeedback onPress={() => this.goSearch()}>
+                <View style={[styleSearchBar.headerSearchBar, {
+                    backgroundColor: '#fff'
+                }]}>
+                    <Image style={styleSearchBar.headerSearchImg}
+                        source={{ uri: require("../images/icon-search-gray") }} />
+                    <Text allowFontScaling={false} style={styleSearchBar.headerSearchInput}>在<Text allowFontScaling={false} style={{ color: '#d0648f' }}>安心淘</Text>中搜索</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        </View>
+    }
+   
+    goSearch(){
+        this.props.navigation.navigate('SearchPage', {});
+    }
+    shareTo(){}
+}
+
+const styleSearchBar = StyleSheet.create({
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: 'space-between',
+        paddingLeft: px(24),
+        paddingRight: px(30),
+        paddingTop: Platform.OS === "ios" ? px(40) : px(10),
+        height: Platform.OS === "ios" ? px(116) : px(76),
+    },
+    back: {
+        width: px(70),
+        height: px(60),
+    },
+    shopLogo: {
+        width: px(56),
+        height: px(56),
+        borderRadius: px(28),
+        overflow: 'hidden',
+        borderWidth: px(2),
+        borderColor: "#fff"
+    },
+    headerShareImg: {
+        width: px(28),
+        height: px(28),
+        borderRadius: px(14),
+        borderWidth: px(1),
+        borderColor: '#efefef',
+        overflow: 'hidden',
+        position: 'absolute',
+        left: px(40),
+        top: px(30)
+    },
+    headerSearchBar: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderRadius: px(35),
+        justifyContent: "center",
+        height: px(56),
+        overflow: 'hidden',
+        marginLeft: px(10)
+    },
+    headerSearchImg: {
+        marginLeft: px(16),
+        width: px(28),
+        height: px(28),
+        marginRight: px(8)
+    },
+    headerSearchInput: {
+        width: px(570),
+        color: "#b2b3b5",
+        fontSize: px(26),
+    },
+    modalHead: {
+        alignItems: 'center',
+        flexDirection: 'column',
+        height: px(169),
+        paddingLeft: px(145),
+        paddingRight: px(145),
+        paddingTop: px(53)
+    },
+    modalTxt1: {
+        fontSize: px(42),
+        color: '#d0648f',
+        fontWeight: '900'
+    },
+    modalTxt2: {
+        fontSize: px(26),
+        color: '#858385',
+        textAlign: 'center',
+        marginTop: px(10),
+        lineHeight: px(30)
+    }
+});
+
 export default class extends React.Component {
     constructor(props) {
         super(props);
@@ -493,6 +599,9 @@ export default class extends React.Component {
 
     render() {
         return <View style={{ flex: 1 }}>
+            <View style={styles.headerView}>
+                <SearchHeader navigation={this.props.navigation} />
+            </View>
             <View style={styles.pageView}>
                 <FlatList ref="flatlist"
                     refreshing={this.state.refreshing}
@@ -638,6 +747,13 @@ export default class extends React.Component {
 }
 
 const styles = StyleSheet.create({
+    headerView: {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        zIndex: 100,
+    },
     pageView: {
         flex: 1,
         width: deviceWidth,
