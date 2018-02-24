3b85604adeff302bb2e6a35982adc99530b11947
郭方超 <guofangchao@daling.com>
2018-02-24 15:57:33 +0800

    添加banner

--- src/home/index.js
+++ src/home/index.js
@@ -7,12 +7,13 @@ import {
     Text,
     Dimensions,
     TouchableWithoutFeedback,
-    ImageBackground
+    ImageBackground,
+    Image
 } from 'react-native';
 import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
 import px from '../utils/px'
-import swiper from 'react-native-swiper'
+import Swiper from 'react-native-swiper'
 import toast from '../utils/toast'
 
 const deviceWidth = Dimensions.get('window').width;
@@ -20,7 +21,7 @@ const deviceWidth = Dimensions.get('window').width;
 /**
  * Banner和快捷入口
  */
-class MyBanner extends React.Component{
+class MyBanner extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -29,44 +30,93 @@ class MyBanner extends React.Component{
             status: false,
         }
     }
-    render(){
-        return <View>
-
+    render() {
+        return <View style={{ flex: 1 }}>
+            {this.state.banners.length === 1 && <TouchableWithoutFeedback onPress={() => this.onPressRow(this.state.banners[0])}>
+                <Image style={{
+                    width: deviceWidth, height: px(480)
+                }} source={{ uri: this.state.banners[0].showImg }} resizeMode="contain" resizeMethod="scale" />
+            </TouchableWithoutFeedback>}
+            {this.state.banners.length > 1 && <View
+                style={{ height: px(480) }}>
+                <Swiper autoplay={true} >
+                    {this.state.banners.map((item, index) => <View key={index}>
+                        <Image resizeMode="cover" resizeMethod="scale"
+                            source={{ uri: item.showImg }}
+                            style={{ width: px(750), height: px(480) }} />
+                    </View>)}
+                </Swiper>
+            </View>}
+            
         </View>
     }
-    componentDidMount(){
-
+    renderRow = (item) => {
+        if (item === undefined) return null
+        return <Image resizeMode="contain" resizeMethod="scale"
+            source={{ uri: item.showImg }}
+            style={{ width: px(750), height: px(480) }} />
     }
+    componentDidMount() {
+        this.refresh();
+    }
+
     /**
      * 刷新列表内容
      */
-    async refresh(){
-
+    async refresh() {
+        try {
+            let res = await request.get(`/banner/findBannerAndQuickList.do?categoryId=`);
+            if (res) {
+                this.setState({
+                    banners: res.bannerList || [],
+                    quicks: res.quickList || []
+                })
+            }
+        } catch (e) {
+            toast(e.message);
+        }
+    }
+    /**
+     * 点击事件
+     * @param {*} e 
+     */
+    onPressRow(e) {
+        this.getDetail(e.contextType, e.context, e.title, e.showImg)
+    }
+    getDetail(type, context, title, shareImg) {
+        type == 1 && this.props.navigation.navigate('DetailPage', {
+            sku: context
+        });
+        type == 3 &&
+            this.props.navigation.navigate('HtmlViewPage', {
+                webPath: context,
+                img: shareImg
+            });
     }
 }
 
 export default class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
     render() {
-        return <View style={{ flex: 1, backgroundColor: "#fff" }}>
+        return <View style={{ flex: 1 }}>
             <View style={styles.pageView}>
-                <TouchableWithoutFeedback onPress={this.test}>
-                    <Text>测试按钮</Text>
-                </TouchableWithoutFeedback>
+                <MyBanner />
             </View>
         </View>
     }
-    async componentDidMount(){
-       
-    }
-    test(){
-        toast("测试弹出的toast")
+    async componentDidMount() {
+
     }
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     pageView: {
         flex: 1,
         width: deviceWidth,
-        marginTop: 100,
     },
 })
