930422659e4b28ca1ad360d875effdbb95a8cdc4
郭方超 <guofangchao@daling.com>
2018-02-25 17:10:05 +0800

    自定义顶部组件

--- src/component/header.js
+++ src/component/header.js
@@ -8,21 +8,63 @@ import {
     TouchableWithoutFeedback,
     TouchableOpacity,
     ImageBackground,
-    Image
+    Image,
+    Platform
 } from 'react-native';
 import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
 import px from '../utils/px'
 import toast from '../utils/toast'
 
-export default class extends React.Component{
+export default class extends React.Component {
 
-    render(){
-        return <View>
-            <Text>展示详情页</Text>
+    static defaultProps = {
+        title : ""
+    }
+    render() {
+        return <View style={topStyles.header}>
+            {Platform.OS == 'ios' && <View style={topStyles.topBox}></View>}
+            <View style={[topStyles.bar, this.props.boxStyles]}>
+                <View style={topStyles.leftBtn}>
+                    <TouchableOpacity style={topStyles.back}
+                        onPress={() => this.props.navigation.goBack()}>
+                        <View style={{ height: px(60), justifyContent: "center" }}>
+                            <Image source={{ uri: require('../images/icon-back') }}
+                                style={{ width: px(40), height: px(40) }} />
+                        </View>
+                    </TouchableOpacity>
+                    {this.props.leftBtn}
+                </View>
+                <Text style={topStyles.title}>{this.props.title}</Text>
+                <View style={topStyles.rightBtn}>
+                    {this.props.rightBtn}
+                </View>
+            </View>
         </View>
     }
 }
 const topStyles = StyleSheet.create({
-
+    header: {
+        backgroundColor: '#fbfafc',
+        width: px(750)
+    },
+    topBox: {
+        width: px(750),
+        height: px(20)
+    },
+    bar: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderBottomColor: "#ddd",
+        borderBottomWidth: 1,
+        height: px(90),
+        paddingTop: px(10)
+    },
+    title: {
+        flex: 1,
+        fontSize: px(30),
+        textAlign: "center",
+        color: "#252426"
+    },
 });
--- src/detail/goods.js
+++ src/detail/goods.js
@@ -14,11 +14,13 @@ import { log, logWarm, logErr } from '../utils/log'
 import request from '../utils/request'
 import px from '../utils/px'
 import toast from '../utils/toast'
+import Header from '../component/header'
 
 export default class extends React.Component{
 
     render(){
         return <View>
+            <Header navigation={this.props.navigation} title="详情页" />
             <Text>展示详情页</Text>
         </View>
     }
