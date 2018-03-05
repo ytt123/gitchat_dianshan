2f40caf1b51d9027920aafd7bf41967e4ee8c30f
郭方超 <guofangchao@daling.com>
2018-03-05 18:32:10 +0800

    简单的webview

---
+++ src/home/browser.js
@@ -0,0 +1,63 @@
+'use strict';
+
+import React from 'react';
+import {
+    StyleSheet,
+    Image,
+    View,
+    WebView
+} from 'react-native';
+
+import Header from '../component/header'
+import { log, logWarm, logErr } from '../utils/log'
+
+
+export class extends React.Component {
+
+    constructor(props) {
+        super(props);
+        let urls = decodeURIComponent(this.props.navigation.state.params.webPath || '');
+        if (urls.indexOf('?') > 0) {
+            urls += '&t=' + Date.now();
+        } else {
+            urls += '?t=' + Date.now();
+        }
+        this.state = {
+            webpath: urls,
+            title: ''
+        }
+    }
+    render() {
+        return <View style={{ flex: 1 }}>
+            <Header title={this.state.title} />
+            <WebView
+                //添加一个引用
+                ref="webview"
+                //设置浏览地址
+                source={{ uri: this.state.webpath }}
+                //启用安卓的js功能
+                javaScriptEnabled={true}
+                //正常的滚动停止速度
+                decelerationRate="normal"
+                //允许https和http一起使用
+                mixedContentMode="always"
+                //显示loading动画
+                startInLoadingState={true}
+                //去掉dom本地存储
+                domStorageEnabled={false}
+                //开启缩放
+                scalesPageToFit={true}
+                //网页互动消息通知
+                onMessage={(t) => this.onMessage(t)}
+                onLoad={(e) => {
+                    this.loaded()
+                    log('网页加载成功', e.nativeEvent)
+                }}
+                onError={(e) => {
+                    log('网页加载失败', e.nativeEvent)
+                }} />
+        </View>
+    }
+    onMessage() { }
+    loaded() { }
+}
