a94e9a995c71b9e42eb66b3ee1e8d654c5e29c05
郭方超 <guofangchao@daling.com>
2018-03-12 19:02:12 +0800

    手动链接微信支付

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import Pages from './src';
+import {registerApp} from 'react-native-wechat';
+
+registerApp('wxfa333b610f00ded2')
 //启动
 AppRegistry.registerComponent('anxintao', () => Pages);
