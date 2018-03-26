cb29db4a3af7d2eb965efa25aaadff0407952ec2
郭方超 <guofangchao@daling.com>
2018-03-26 11:19:01 +0800

    添加第三方组件

--- android/app/src/main/java/com/anxintao/MainApplication.java
+++ android/app/src/main/java/com/anxintao/MainApplication.java
@@ -3,6 +3,7 @@ package com.anxintao;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cn.reactnative.modules.update.UpdatePackage;
 import com.theweflex.react.WeChatPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.facebook.react.ReactNativeHost;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new UpdatePackage(),
             new WeChatPackage(),
             new FastImageViewPackage(),
             new WeChatPackage(),
